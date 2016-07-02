b3f03eda9b6df5b33feac3830d698ae31ce436d0
Twoure <twoure@aol.com>
2016-07-01 20:49:49 -0500

    Updated decryption to use Plex Framework, removed unused code

--- Contents/Libraries/Shared/sha2/__init__.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python
-
-__author__ = 'Thomas Dixon'
-__license__ = 'MIT'
-
-from sha224 import sha224
-from sha256 import sha256
-from sha384 import sha384
-from sha512 import sha512
--- Contents/Libraries/Shared/sha2/sha224.py
+++
@@ -1,1 +0,0 @@
-#!/usr/bin/python__author__ = 'Thomas Dixon'__license__ = 'MIT'from sha256 import sha256def new(m=None):    return sha224(m)class sha224(sha256):    _h = (0xc1059ed8L, 0x367cd507L, 0x3070dd17L, 0xf70e5939L,          0xffc00b31L, 0x68581511L, 0x64f98fa7L, 0xbefa4fa4L)    _output_size = 7    digest_size = 28
